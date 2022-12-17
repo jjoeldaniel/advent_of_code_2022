974f52474fef4b0b4a174dab6c03eadc2d38f065
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-17 05:30:48 -0800

    Added both solutions, now uses list

--- day1/day1.py
+++ day1/day1.py
@@ -1,26 +1,18 @@
 f = open("day1\input.txt", "r")
-dict = {}
+list = []
 calories = 0
-i = 0
 
 for line in f:
 
     if (line == '\n'):
-        i += 1
+        list.append(calories)
         calories = 0
         continue
 
     calories += int(line)
-    dict.update({i : calories})  
 
 f.close()
+list.sort(reverse=True)
 
-max = dict[0]
-high = 0
-
-for k, v in dict.items():
-    if v > max:
-        max = v
-        high = k
-
-print(dict[high])
+print(list[0])
+print(list[0] + list[1] + list[2])
