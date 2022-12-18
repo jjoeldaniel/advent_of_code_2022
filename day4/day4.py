f38919316b46dd14dba8f41fc65ca4cf39879e5d
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-18 06:35:07 -0800

    finished both parts

--- day4/day4.py
+++ day4/day4.py
@@ -1,16 +1,19 @@
 import re
 
 i = 0
+k = 0
 
 for line in open("input.txt", "r"):
     numbers = re.split(r'-|,', line)
     numbers = [item.strip() for item in numbers]
 
-    x = set(range(int(numbers[0]), int(numbers[1])))
-    y = set(range(int(numbers[2]), int(numbers[3])))
+    x = set(range(int(numbers[0]), int(numbers[1]) + 1))
+    y = set(range(int(numbers[2]), int(numbers[3]) + 1))
+    z = x & y
 
-    # TODO: fix
     if x.issubset(y) or y.issubset(x):
         i += 1
+    if len(z) > 0:
+        k += 1
 
-print(i)
+print(i, k)
